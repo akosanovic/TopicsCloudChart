8233418e1a96d9149ee28d410289dba2a10cb51a
Aleksandra Kosanovic <kiselo.slatko@gmail.com>
2022-05-18 01:28:37 +0200

    set font size

--- src/WordCloudChart.js
+++ src/WordCloudChart.js
@@ -3,7 +3,7 @@ import Highcharts from 'highcharts'
 import HighchartsReact from 'highcharts-react-official'
 import wordcloud from 'highcharts/modules/wordcloud'
 
-export default function WordCloudChart({ chartData, onTopicUpdate }) {
+export default function WordCloudChart({ chartData, topicUpdate }) {
 
     var selectedTopicId = null;
     var [data, setData] = useState();
@@ -13,25 +13,44 @@ export default function WordCloudChart({ chartData, onTopicUpdate }) {
 
 
     function handleOnTopicSelect() {
-        return onTopicUpdate(selectedTopicId);
+        return topicUpdate(selectedTopicId);
     }
 
+    let weightsArray = [];
+
+    Highcharts.seriesTypes.wordcloud.prototype.deriveFontSize = function (relativeWeight) {
+        // relativeWeight = relativeWeight * 10;
+        var maxFontSize = 8;
+        // Will return a fontSize between 0px and 25px.
+        let weight = Math.floor(maxFontSize * relativeWeight);
+
+        // !weightsArray.includes(weight) ? weightsArray.push(weight) : console.log('exists');
+
+        weightsArray.push(weight * 12.5)
+
+        // console.log('final weight ', weightsArray.includes(weight));
+        return weight ? weight * 10 : 8;
+    };
+
+    console.log('weightsArray ', weightsArray);
 
     var options = {
         series: [{
             type: 'wordcloud',
             data,
-            name: 'Occurrences',
+            name: 'Occurrences', //Todo: change the name on tooltip
+            rotation: { // Disable word rotation
+                from: 0,
+                to: 0,
+            },
+            style: {
+                fontFamily: 'Verdana',
+            },
             point: {
-                events: {
-                    click: function (event, asdf) {
-
+                events: { 
+                    click: function (event) { // Dispatch Event: show info box on topic click 
                         selectedTopicId = event.point.index;
                         handleOnTopicSelect();
-
-                        // console.log('event ', event);
-                        // console.log('asdf ', event.point.index, ' ', event.point.name)
-                        // alert('Category: ' + this.category + ', value: ' + this.y);
                     }
                 }
             }
