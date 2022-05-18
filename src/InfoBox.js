504443ce8b93d6896c1c89a06b8adbfe50ed365b
Aleksandra Kosanovic <kiselo.slatko@gmail.com>
2022-05-18 21:48:26 +0200

    fix conditional logic display

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -3,31 +3,29 @@ import InfoBoxEmptyPlaceholder from './InfoBoxEmptyPlaceholder'
 
 export default function InfoBox({ selectedTopic }) {
 
+    if (!selectedTopic) {
+        return (<InfoBoxEmptyPlaceholder placeholderText={"Click on the topic to see more info"} />)
+    }
     return (
 
         <>
-            {!selectedTopic && <InfoBoxEmptyPlaceholder placeholderText={"Click on the topic to see more info"} />}
-
-            {selectedTopic && <>
-
             <h2>Information on the topic: {selectedTopic.label} </h2>
 
-            <p>Total Mentions: {selectedTopic.volume ? selectedTopic.volume : 0}</p>
+            <p>Total Mentions: &nbsp; {selectedTopic.volume ? selectedTopic.volume : 0}</p>
 
 
-                <p data-testid="positive">Positive Mentions:
-                    <span className='text-green'>
-                        {selectedTopic.sentiment?.positive ? selectedTopic.sentiment.positive : 0}
-                    </span>
-                </p>
-                <p>Neutral Mentions: {selectedTopic.sentiment?.neutral ? selectedTopic.sentiment.neutral : 0}</p>
-                <p data-testid="negative">Negative Mentions:
-                    <span className='text-red'>
-                        {selectedTopic.sentiment?.negative ? selectedTopic.sentiment.negative : 0}
-                    </span>
-                </p>
-            </>
-            }
+            <p data-testid="positive">Positive Mentions: &nbsp;
+                <span className='text-green'>
+                    {selectedTopic.sentiment?.positive ? selectedTopic.sentiment.positive : 0}
+                </span>
+            </p>
+            <p>Neutral Mentions: {selectedTopic.sentiment?.neutral ? selectedTopic.sentiment.neutral : 0}</p>
+            <p data-testid="negative">Negative Mentions: &nbsp;
+                <span className='text-red'>
+                    {selectedTopic.sentiment?.negative ? selectedTopic.sentiment.negative : 0}
+                </span>
+            </p>
         </>
+
     )
 }
