396aebe462b202a0bae00f3c5a0b2170fdb91057
Aleksandra Kosanovic <kiselo.slatko@gmail.com>
2022-05-29 18:10:30 +0200

    Updates

--- src/Config.js
+++
@@ -1,5 +0,0 @@
-export const COLOR_RED = "#ef4444";
-export const COLOR_GREEN = "#10b981";
-export const COLOR_GREY = "#6b7280";
-
-export const NO_OF_SIZE_VARIENTS = 6;
--- src/InfoBoxEmptyPlaceholder.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function InfoBoxEmptyPlaceholder({ placeholderText }) {
-    return (
-        <div>
-            <h4>{placeholderText}</h4>
-        </div>
-    )
-}
